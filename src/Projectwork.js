a2698a069e6bc27a71f00a7a82e7d5540881da99
ashwini-chaudhar <ashwini.chaudhar7@gmail.com>
2022-11-04 17:27:29 +0530

    changes

--- src/Projectwork.js
+++ src/Projectwork.js
@@ -6,42 +6,39 @@ export default function Projectwork(){
     <div className="d-felx flex-colomn">
         <div className="education-one my-4 justify-content-between d-flex flex-row">
             <div className="d-flex flex-column new-one">
-                <span className="univercity-name">pune-univercity {""}</span>
-                <span className="univercity-degree">Bachelor of computar science {""}</span>
+               <span className="univercity-name">Travel and Tourism Management  {""}</span>
             </div>
            {""}
             <div>
-                <span className="year-passout">2019 -2021</span>
+                <span className="year-passout">2019 -2020</span>
             </div>
             </div>
             <div className="education-one my-4 justify-content-between d-flex flex-row">
             <div className="d-flex flex-column new-one">
-                <span className="univercity-name">pune-univercity {""}</span>
-                <span className="univercity-degree">Bachelor of computar science {""}</span>
+            <span className="univercity-name">Assignment {""}</span>
             </div>
            {""}
             <div>
-                <span className="year-passout">2019 -2021</span>
+                <span className="year-passout">2021-2021</span>
             </div>
             </div>
             <div className="education-one my-4 justify-content-between d-flex flex-row">
             <div className="d-flex flex-column new-one">
-                <span className="univercity-name">pune-univercity {""}</span>
-                <span className="univercity-degree">Bachelor of computar science {""}</span>
+            
+            <span className="univercity-name">Portfolio Website {""}</span>
             </div>
            {""}
             <div>
-                <span className="year-passout">2019 -2021</span>
+                <span className="year-passout">2022 -2022</span>
             </div>
             </div>
             <div className="education-one my-4 justify-content-between d-flex flex-row">
             <div className="d-flex flex-column new-one">
-                <span className="univercity-name">pune-univercity {""}</span>
-                <span className="univercity-degree">Bachelor of computar science {""}</span>
+            <span className="univercity-name">Landing Page {""}</span>
             </div>
            {""}
             <div>
-                <span className="year-passout">2019 -2021</span>
+                <span className="year-passout">2022 -2022</span>
             </div>
             </div>
             </div>
